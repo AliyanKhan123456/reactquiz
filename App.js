4f47c15419b30120720226fcaec338f8efb9bfce
AliyanKhan123456 <110288715+AliyanKhan123456@users.noreply.github.com>
2023-02-18 13:40:24 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import logo from "./logo.svg";
+import { useState } from "react";
+import "./App.css";
+import Router from "./router/router";
+
+function App() {
+  return <Router />;
+}
+
+export default App;
